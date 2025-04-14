6be7d2c703eb86a85c819d094ce9ba57f1cc5248
11fre <11free11@mail.ru>
2025-04-14 08:53:04 +0300

    OOP lesson
    Practic

---
+++ inheritance_github.py
@@ -0,0 +1,23 @@
+class Mammal():
+    className = 'Mammal'
+
+class Dog(Mammal):
+    species = 'Canis lupus'
+
+dog = Dog()
+print(dog.className) # Mammal
+print('-' * 20)
+
+
+class Horse():
+    isHorse = True
+
+class Donkey():
+    isDonkey = True
+
+class Mule(Horse, Donkey):
+    pass
+
+mule = Mule()
+print(mule.isHorse) # True
+print(mule.isDonkey) # True
